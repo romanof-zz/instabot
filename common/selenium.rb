42014740b6d997503a4b64b9871bec2c19015ecd
Romanov <romanovs@amazon.com>
2017-08-03 18:11:18 -0700

    explicitly setting port.

--- common/selenium.rb
+++ common/selenium.rb
@@ -6,11 +6,12 @@ def driver
     # @driver = Selenium::WebDriver.for :remote, url: "http://127.0.0.1:9515", desired_capabilities: :chrome
     Selenium::WebDriver.logger.level = :debug
     Selenium::WebDriver.logger.output = 'selenium.log'
-    
+
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--disable-popup-blocking')
     options.add_argument('--disable-translate')
+    options.add_argument('--port=9515')
     @driver = Selenium::WebDriver.for :chrome, options: options
   end
 
