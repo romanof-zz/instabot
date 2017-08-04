95076311bc7e3fcb7e7bfc08478a6e83a920bb8e
Romanov <romanovs@amazon.com>
2017-08-03 20:34:12 -0700

    exporting env for cron.

--- likes.sh
+++ likes.sh
@@ -1,2 +1,7 @@
+export PATH=$PATH:/home/ubuntu/.rvm/rubies/ruby-2.3.3/bin/
+export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
+export PATH=$PATH:/usr/local/bin
+export DISPLAY=:0
+
 ruby console.rb --asUser=roman0f --operation=likelatest
 ruby console.rb --asUser=yulyassana --operation=likelatest
