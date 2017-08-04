750359c7361402930e0c5dcad362c78a6abc6abb
Romanov <romanovs@amazon.com>
2017-08-03 18:05:20 -0700

    adding driver logging.

--- common/selenium.rb
+++ common/selenium.rb
@@ -4,6 +4,9 @@ require "selenium-webdriver"
 def driver
   if @driver.nil?
     # @driver = Selenium::WebDriver.for :remote, url: "http://127.0.0.1:9515", desired_capabilities: :chrome
+    Selenium::WebDriver.logger.level = :debug
+    Selenium::WebDriver.logger.output = 'selenium.log'
+    
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--disable-popup-blocking')
