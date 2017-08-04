c7093626e24cfcd7dc7b5837038d0fdca6d33e9c
Romanov <romanovs@amazon.com>
2017-08-04 08:24:24 -0700

    fixing settypes.

--- scripts/settypes.rb
+++ scripts/settypes.rb
@@ -3,14 +3,18 @@ require_relative "../common/selenium"
 require_relative "../common/db"
 
 def set_follower_types
-  get_untyped_followers(10).each do |follower|
+  get_untyped_followers(100).each do |follower|
     name = follower["name"]
     driver.navigate.to "https://www.instagram.com/#{name}/"
-    Selenium::WebDriver::Wait.new(timeout: 30).until { driver.find_element(xpath: "//main//div//img") }
+    Selenium::WebDriver::Wait.new(timeout: 60).until { driver.find_element(xpath: "//main//div//img") }
 
-    type = false
-    type = "private" if driver.find_element(xpath: "//main/article/div/h2")
-    type = "public" if driver.find_element(xpath: "//main//div//a//img")
+    type = "public"
+    begin
+      element = driver.find_element(xpath: "//main/article/div/h2")
+      type = "private" if element.text() == "This Account is Private"
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    end
+
     puts " #{name} - #{type}"
 
     if type
