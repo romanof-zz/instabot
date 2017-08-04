a9310252d8546c364eb60af0c8445a7c543fbd8d
Romanov <romanovs@amazon.com>
2017-08-03 17:56:10 -0700

    adding some install scripts and changing to chrome.

--- common/selenium.rb
+++ common/selenium.rb
@@ -2,6 +2,14 @@
 require "selenium-webdriver"
 
 def driver
-  @driver = Selenium::WebDriver.for :remote, url: "http://127.0.0.1:9515", desired_capabilities: :chrome if @driver.nil?
+  if @driver.nil?
+    # @driver = Selenium::WebDriver.for :remote, url: "http://127.0.0.1:9515", desired_capabilities: :chrome
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-popup-blocking')
+    options.add_argument('--disable-translate')
+    @driver = Selenium::WebDriver.for :chrome, options: options
+  end
+
   @driver
 end
---
+++ ssh.instabot.sh
@@ -0,0 +1,1 @@
+ssh -i romanof.pem ubuntu@ec2-54-197-29-247.compute-1.amazonaws.com
