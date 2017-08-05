872f2bf6158e70cfd68141cd563a49b000c522fd
Romanov <romanovs@amazon.com>
2017-08-05 08:39:32 -0700

    fixing deleted accounts processing.

--- scripts/settypes.rb
+++ scripts/settypes.rb
@@ -6,13 +6,18 @@ def set_follower_types
   get_untyped_followers(100).each do |follower|
     name = follower["name"]
     driver.navigate.to "https://www.instagram.com/#{name}/"
-    Selenium::WebDriver::Wait.new(timeout: 60).until { driver.find_element(xpath: "//main//div//img") }
+    Selenium::WebDriver::Wait.new(timeout: 60).until { driver.find_element(xpath: "//header//a[@href=\"/\"]") }
 
     type = "public"
     begin
       element = driver.find_element(xpath: "//main/article/div/h2")
       type = "private" if element.text() == "This Account is Private"
     rescue Selenium::WebDriver::Error::NoSuchElementError
+      begin
+        element = driver.find_element(xpath: "//div/h2")
+        type = "deleted" if element.text() == "Sorry, this page isn't available."
+      rescue Selenium::WebDriver::Error::NoSuchElementError
+      end
     end
 
     puts " #{name} - #{type}"
