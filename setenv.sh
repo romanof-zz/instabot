9fe62db0c6139551875f4916e88d5ba501ba0ef4
Romanov <romanovs@amazon.com>
2017-08-06 10:28:31 -0700

    creating set env sh.

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,4 @@
-(use "git push" to publish your local commits)
 export PATH=$PATH:/home/ubuntu/.rvm/rubies/ruby-2.3.3/bin/
 export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
 export PATH=$PATH:/usr/local/bin
 export DISPLAY=:0
-
-ruby console.rb --operation=unfollow --followers=253 --following=190 --asUser=roman0f
