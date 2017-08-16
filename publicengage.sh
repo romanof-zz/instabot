59b78ec035be07954039f1a5987d7492afd92b96
Romanov <romanovs@amazon.com>
2017-08-16 10:16:15 -0700

    cleanup sh scripts.

--- publicengage.sh
+++
@@ -1,6 +0,0 @@
-export PATH=$PATH:/home/ubuntu/.rvm/rubies/ruby-2.3.3/bin/
-export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
-export PATH=$PATH:/usr/local/bin
-export DISPLAY=:0
-ruby console.rb --operation=publicengage --asUser=roman0f --lang=rus
-ruby console.rb --operation=publicengage --asUser=yulyassana --lang=rus
--- unfollow.sh
+++
@@ -1,5 +0,0 @@
-export PATH=$PATH:/home/ubuntu/.rvm/rubies/ruby-2.3.3/bin/
-export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
-export PATH=$PATH:/usr/local/bin
-export DISPLAY=:0
-ruby console.rb --operation=unfollow --asUser=roman0f --followers=250 --following=107
