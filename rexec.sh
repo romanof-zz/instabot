5317a7cd4b11b1c46f080253658a4958416bd5ae
Romanov <romanovs@amazon.com>
2017-08-21 11:33:55 -0700

    removing ruby mem setting as they seem to slow down ec2 even more.

--- rexec.sh
+++ rexec.sh
@@ -3,9 +3,9 @@ export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
 export PATH=$PATH:/usr/local/bin
 export DISPLAY=:0
 # export RUBY_HEAP_MIN_SLOTS=600000
-export RUBY_GC_HEAP_INIT_SLOTS=600000
-export RUBY_GC_MALLOC_LIMIT=59000000
+# export RUBY_GC_HEAP_INIT_SLOTS=600000
+# export RUBY_GC_MALLOC_LIMIT=59000000
 # export RUBY_FREE_MIN=100000
-export RUBY_GC_HEAP_FREE_SLOTS=100000
+# export RUBY_GC_HEAP_FREE_SLOTS=100000
 
 ruby console.rb "$@"
