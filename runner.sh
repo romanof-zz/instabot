32b015ab2ad222824eca58534fbb86c30bb51c55
Romanov <romanovs@amazon.com>
2017-08-30 00:58:27 -0700

    removing cron and making runner invoke itself.

--- runner.sh
+++ runner.sh
@@ -1,9 +1,22 @@
-sh rexec.sh --operation=settypes
-sh rexec.sh --operation=likelatest --asUser=roman0f
-sh rexec.sh --operation=privateengage --asUser=roman0f
-sh rexec.sh --operation=publicengage --asUser=roman0f
-sh rexec.sh --operation=requestfollow --asUser=roman0f
-sh rexec.sh --operation=likelatest --asUser=yulyassana
-sh rexec.sh --operation=privateengage --asUser=yulyassana
-sh rexec.sh --operation=publicengage --asUser=yulyassana
-sh rexec.sh --operation=requestfollow --asUser=yulyassana
+export PATH=$PATH:/home/ubuntu/.rvm/rubies/ruby-2.3.3/bin/
+export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
+export PATH=$PATH:/usr/local/bin
+export DISPLAY=:0
+# export RUBY_HEAP_MIN_SLOTS=600000
+# export RUBY_GC_HEAP_INIT_SLOTS=600000
+# export RUBY_GC_MALLOC_LIMIT=59000000
+# export RUBY_FREE_MIN=100000
+# export RUBY_GC_HEAP_FREE_SLOTS=100000
+
+ruby console.rb --operation=settypes
+ruby console.rb --operation=likelatest --asUser=roman0f
+ruby console.rb --operation=privateengage --asUser=roman0f
+ruby console.rb --operation=publicengage --asUser=roman0f
+ruby console.rb --operation=requestfollow --asUser=roman0f
+ruby console.rb --operation=likelatest --asUser=yulyassana
+ruby console.rb --operation=privateengage --asUser=yulyassana
+ruby console.rb --operation=publicengage --asUser=yulyassana
+ruby console.rb --operation=requestfollow --asUser=yulyassana
+
+sleep 10m
+sh runner.sh >> app.log 2>&1
