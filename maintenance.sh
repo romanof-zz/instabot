a841c00410d1740a575178632333e3b5c783baa4
Romanov <romanovs@amazon.com>
2017-09-28 13:39:01 +0800

    relaunching a full maintenance.

--- maintenance.sh
+++ maintenance.sh
@@ -2,7 +2,9 @@ export PATH=$PATH:/home/ubuntu/.rvm/rubies/ruby-2.3.3/bin/
 export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
 export PATH=$PATH:/usr/local/bin
 
-# pkill -f runner
-# pkill -f ruby
-# sh runner.sh >> app.log 2>&1 &
+pkill -f runner.sh
+pkill -f ruby
+rm *.log
+rm screenshots/*.png
+sh runner.sh >> app.log 2>&1 &
 ruby ipswap.rb
