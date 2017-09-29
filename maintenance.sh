6db7d961dc5679a273e950f480bed4792ddfa718
Romanov <romanovs@amazon.com>
2017-09-29 20:55:47 +0800

    reverse maintanance to simple ip swap.

--- maintenance.sh
+++ maintenance.sh
@@ -2,9 +2,4 @@ export PATH=$PATH:/home/ubuntu/.rvm/rubies/ruby-2.3.3/bin/
 export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
 export PATH=$PATH:/usr/local/bin
 
-pkill -f runner.sh
-pkill -f ruby
-rm *.log
-rm screenshots/*.png
-sh runner.sh >> app.log 2>&1 &
 ruby ipswap.rb
