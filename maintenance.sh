09054d5f7de45b32c8281a550c178623022a8f94
Romanov <romanovs@amazon.com>
2017-10-08 16:07:05 +0800

    adding bundle to path.

--- maintenance.sh
+++ maintenance.sh
@@ -1,5 +1,6 @@
 export PATH=$PATH:/home/ubuntu/.rvm/rubies/ruby-2.3.3/bin/
-export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
+export PATH=$PATH:/home/ubuntu/.rvm/gems/ruby-2.3.3/bin
 export PATH=$PATH:/usr/local/bin
+export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
 
 bundle exec ruby ipswap.rb
--- runner.sh
+++ runner.sh
@@ -1,6 +1,7 @@
 export PATH=$PATH:/home/ubuntu/.rvm/rubies/ruby-2.3.3/bin/
-export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
+export PATH=$PATH:/home/ubuntu/.rvm/gems/ruby-2.3.3/bin
 export PATH=$PATH:/usr/local/bin
+export GEM_HOME=/home/ubuntu/.rvm/gems/ruby-2.3.3
 export DISPLAY=:0
 # export RUBY_HEAP_MIN_SLOTS=600000
 # export RUBY_GC_HEAP_INIT_SLOTS=600000
